9804dacd8904e1f0e391483cc770798138d3b7bc
John <johntbrooks92@gmail.com>
2020-12-02 11:10:16 -0600

    syntax, types, vars completed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,48 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+//        int myFavoriteNumber = 13;
+//        System.out.println(myFavoriteNumber);
+
+//        String myString = "3.14159";
+//        System.out.println(myString);
+
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+////        x = x + 5;
+
+//        System.out.println(x+5);
+
+
+//        int x = 3;
+//        int y = 4;
+//        System.out.println(y*x);
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+//        x = x / y;
+//        y = y - x;
+        System.out.println(x/y);
+        System.out.println(y-x);
+        System.out.println(x);
+        System.out.println(y);
+
+
+
     }
 }
