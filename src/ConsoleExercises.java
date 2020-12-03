e944b4509c266e17404efd22f4c650c4bd19ea3b
John <johntbrooks92@gmail.com>
2020-12-03 13:38:12 -0600

    initial push controlflow exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,20 +4,20 @@ public class ConsoleExercises {
 
     public static void main(String[] args) {
 
+
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f.\n", pi);
+
         Scanner userInput = new Scanner(System.in);
         System.out.println(userInput);
-//        double pi = 3.14159;
-//        System.out.printf("The value of pi is approximately $%1.0f%n", pi);
 
-
-
-//        System.out.println("Please enter your 3 favorite pokemon:");
-//        String firstPokemon = userInput.next();
-//        String secondPokemon = userInput.next();
-//        String thirdPokemon = userInput.next();
-//        System.out.println(firstPokemon);
-//        System.out.println(secondPokemon);
-//        System.out.println(thirdPokemon);
+        System.out.println("Please enter your 3 favorite pokemon:");
+        String firstPokemon = userInput.next();
+        String secondPokemon = userInput.next();
+        String thirdPokemon = userInput.next();
+        System.out.println(firstPokemon);
+        System.out.println(secondPokemon);
+        System.out.println(thirdPokemon);
 
 //
 //        System.out.println("Where is Waldo?");
