b60f7be607023ee34c22a6ce350c50ea331d5ccf
John <johntbrooks92@gmail.com>
2020-12-03 09:51:49 -0600

    finished console exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -11,15 +11,30 @@ public class ConsoleExercises {
 
 
 
-        System.out.println("Please enter your 3 favorite pokemon:");
-        String firstPokemon = userInput.next();
-        String secondPokemon = userInput.next();
-        String thirdPokemon = userInput.next();
-        System.out.println(firstPokemon);
-        System.out.println(secondPokemon);
-        System.out.println(thirdPokemon);
+//        System.out.println("Please enter your 3 favorite pokemon:");
+//        String firstPokemon = userInput.next();
+//        String secondPokemon = userInput.next();
+//        String thirdPokemon = userInput.next();
+//        System.out.println(firstPokemon);
+//        System.out.println(secondPokemon);
+//        System.out.println(thirdPokemon);
 
+//
+//        System.out.println("Where is Waldo?");
+//        String location = userInput.nextLine();
+//        System.out.println(location);
 
+        System.out.println("Enter the length of the Jupiter classroom");
+        String userLength = userInput.nextLine();
+        System.out.println("Enter the width of the Jupiter classroom");
+        String userWidth = userInput.nextLine();
+        int lengthInput = Integer.parseInt(userLength);
+        int widthInput = Integer.parseInt(userWidth);
+        int roomArea = lengthInput * widthInput;
+        int roomPerimeter = (2*lengthInput) + (2*widthInput);
+
+        System.out.printf("The area of the classroom is %d.%n", roomArea);
+        System.out.printf("The perimeter of the classroom is %d.%n", roomPerimeter);
 
 
     }
