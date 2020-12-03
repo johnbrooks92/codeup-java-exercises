ac916fb678295eaa701ca297d61c6644e4347472
John <johntbrooks92@gmail.com>
2020-12-03 14:03:57 -0600

    loop section exercises completed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,6 +1,32 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
+
+//        for(int i = 5; i <= 15; i++) {
+//            System.out.println("i is " + i);
+//        }
+
+//        int n = 100;
+//        System.out.print("Even Numbers from 0 to "+n+" are: ");
+//        for (int i = 0; i <= n; i++) {
+//            //if number%2 == 0 it means its an even number
+//            if (i % 2 == 0) {
+//                System.out.print(i + " ");
+//            }
+//        }
+
+//        for(int i = 0; i <= 100; i++) { // <--+
+//            if(i % 2 == 1) { //              |
+//                continue; // ----------------+
+//            }
+//            System.out.println(i);
+//        }
+
 
     }
 }
