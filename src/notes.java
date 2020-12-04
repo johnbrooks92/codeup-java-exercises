7f1ba27e7ede1cb209289f381ecfe2c24366edae
John <johntbrooks92@gmail.com>
2020-12-04 14:25:28 -0600

    pre-lec

---
+++ src/notes.java
@@ -0,0 +1,62 @@
+//- What is a Java method?
+
+//Methods are a sequence of statements that perform a specific task.
+//They are analogous to functions in JavaScript, and like JavaScript functions, they commonly accept input, and produce output.
+
+//- Why use additional methods rather than writing all Java code in a main method?
+
+//It makes the code more modular.
+// Easier to test.
+// Simpler to debug.
+
+//- What does it mean to ‘call’ or ‘invoke’ a method?
+
+//It causes the statements in a method to "run"
+
+//When we define a method, we define parameters (inputs to the method), and when we call a function, we call it with arguments (the actual values passed to the method).
+//Arguments can be literal values, variables, or themselves expressions that will be evaluated to a value that is passed to the method.
+//Keep in mind that the data type of the arguments must be the same as the data type of the parameters that the method is defined with.
+
+
+
+
+//What are six parts of a method definition as explained in the curriculum?
+
+//visibility
+//static/instance
+//return type
+//name of method
+//parameters
+//body
+
+
+//In what ways do Java methods differ from JS functions?
+
+//Syntax
+//Methods tied to class or object in Java vs lone functions in JS
+//default values require multiple methods vs syntax in a single function
+//parameter data type is fixed vs flexible
+//methods calls have to relate main method in Java bs no main method
+
+//What is a method signature?
+
+//method name + parameter list
+//add(int x, int y)
+
+
+//What is method overloading?
+
+//Same name but accept different parameters
+
+//ADD EXACT DEFINITION LATER
+
+
+//Will changing the return type of a method overload it?
+//TBD in later lesson
+
+
+//What is recursion and what are the differences/pros/cons compared to iteration?
+
+
+
+//What is a StackOverflowError?
