c3455b26151fb698e8b14eec1caeb20fa24410e5
John <johntbrooks92@gmail.com>
2020-12-07 15:41:56 -0600

    Sophie's exercise added to notes class

--- src/quizandtest.java
+++ src/quizandtest.java
@@ -1,7 +1,48 @@
+import java.util.Scanner;
+
 public class quizandtest {
 
     public static void main(String[] args) {
-        int x = 5 * 4 % 3;
-        System.out.println(x);
+        Scanner sc = new Scanner(System.in);
+        userInteractionFactorial(sc);
     }
+    public static int getInteger(int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) {
+            System.out.println("Not a number!");
+            return getInteger(min, max);
+        }
+        int userInput = sc.nextInt();
+        if (userInput >= min && userInput <= max) {
+            return userInput;
+        } else {
+            System.out.println("Number not in range!");
+            return getInteger(min, max);
+        }
+    }
+        public static long calculateFactorial(int num) {
+            int output = 1;
+            for (int i = 1; i <= num; i += 1) {
+                output *= i;
+            }
+            return output;
+        }
+//
+//
+        public static void userInteractionFactorial(Scanner sc) {
+            boolean willContinue;
+            String userChoice;
+            do {
+                System.out.println("Please enter an integer from 1 to 12");
+                int userInt = getInteger(1, 12);
+                System.out.println(calculateFactorial(userInt));
+                do {
+                    System.out.println("Do you wish to continue? [y/n]: ");
+                    userChoice = sc.next().trim();
+                } while (!userChoice.equalsIgnoreCase("y") & !userChoice.equalsIgnoreCase("n"));
+
+                willContinue = userChoice.equalsIgnoreCase("y");
+            } while (willContinue);
+        }
+
 }
