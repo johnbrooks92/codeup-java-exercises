a26a93f44b7b88ebd5b318c0cc53927003e5c87a
John <johntbrooks92@gmail.com>
2020-12-07 11:19:31 -0600

    methods exercise finished

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.lang.Math;
+import java.util.Random;
 
 public class MethodsExercises {
 
@@ -19,14 +21,15 @@ public class MethodsExercises {
         System.out.println("Product: " + product);
         System.out.println("Quotient: " + quotient);
         System.out.println("Remainder: " + modulus);
-
-
-        System.out.println("Enter a number between 1 and 10.");
+        System.out.println("Enter the \"1\" key to continue ");
         userInput = myScanner.nextInt();
-        getInteger(1, 10, userInput);
-
+        System.out.println(factorial(userInput));
+        System.out.println("Enter any key between \"1\" and \"100\" ");
+        userInput = myScanner.nextInt();
+        System.out.println(dice(userInput));
 
     }
+
 
     public static int Addition(int x, int y) {
         return x + y;
@@ -51,12 +54,29 @@ public class MethodsExercises {
         return x % y;
     }
 
-    public static void getInteger(int min, int max, int userInput) {
 
+    public static int getInteger(int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a key between \"1\" and \"10\": ");
+        int userInput = sc.nextInt();
         if (userInput >= min && userInput <= max) {
-            System.out.println("Valid number entered");
-        } else {
-            System.out.println("Please enter a different number.");
+            return userInput;
         }
+        System.out.println("Invalid input. Try again.");
+        return getInteger(min, max);
+    }
+    public static long factorial(int fact){
+        long userInput = getInteger(1,10);
+        for (long i = 1; i <= userInput; i++){
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static String dice(int sides){
+        Random random = new Random();
+        int roll1 = random.nextInt(sides) + 1;
+        int roll2 = random.nextInt(sides) + 1;
+        return "Your first roll was " + roll1 + " and your second roll was " + roll2;
     }
 }
