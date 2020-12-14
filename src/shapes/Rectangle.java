3eaaf2a23678633b75f54c1b61b0cd81b3c3cfa5
John <johntbrooks92@gmail.com>
2020-12-14 14:02:49 -0600

    reformatted code

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -4,16 +4,16 @@ public class Rectangle {
     protected int length;
     protected int width;
 
-    public Rectangle(int l, int w){
+    public Rectangle(int l, int w) {
         this.length = l;
         this.width = w;
     }
 
-    public int getArea(){
+    public int getArea() {
         return this.length * this.width;
     }
 
-    public int getPerimeter(){
+    public int getPerimeter() {
         return (this.length * 2) + (this.width * 2);
     }
 
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,17 +1,19 @@
 package shapes;
 
-public class Square extends Rectangle{
+public class Square extends Rectangle {
 
-    public Square (int s){
+    public Square(int s) {
         super(s, s);
     }
+
     @Override
-    public int getArea(){
+    public int getArea() {
         System.out.println("Calculating Area:");
         return this.width * this.length;
     }
+
     @Override
-    public int getPerimeter(){
+    public int getPerimeter() {
         System.out.println("Calculating Perimeter:");
         return this.length * 4;
     }
